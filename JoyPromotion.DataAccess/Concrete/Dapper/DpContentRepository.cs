930521e0ecafcd827f2022dcd220759971fd2223
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-19 17:22:20 +0300

    WebUI & DataAccess Content Added

---
+++ JoyPromotion.DataAccess/Concrete/Dapper/DpContentRepository.cs
@@ -0,0 +1,14 @@
+﻿using JoyPromotion.DataAccess.Abstract;
+using JoyPromotion.Entities.Concrete;
+using JoyPromotion.Shared.DataAccess.Dapper;
+using System.Data;
+
+namespace JoyPromotion.DataAccess.Concrete.Dapper
+{
+    public class DpContentRepository : DpGenericRepository<Content>, IContentRepository
+    {
+        public DpContentRepository(IDbConnection dbConnection) : base(dbConnection)
+        {
+        }
+    }
+}
