fa036e3865d401e9c38bbb6ac917569b5891f6e3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-21 00:19:19 +0300

    Image Added

---
+++ JoyPromotion.Web/Utils/ImageFile.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace JoyPromotion.Web.Utils
+{
+    public class ImageFile
+    {
+        public void Upload(IFormFile image, out string imageUrl)
+        {
+            var imageName = Guid.NewGuid() + Path.GetExtension(image.FileName);
+            var path = Directory.GetCurrentDirectory() + "/wwwroot/img/" + imageName;
+            var stream = new FileStream(path, FileMode.Create);
+            image.CopyTo(stream);
+            imageUrl = imageName;
+        }
+    }
+}
