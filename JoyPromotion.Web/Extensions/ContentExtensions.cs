ac43f6630cd066dd1cb56e0094b71c0bd793980f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-21 14:37:12 +0300

    Content Description Added

---
+++ JoyPromotion.Web/Extensions/ContentExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace JoyPromotion.Web.Extensions
+{
+    public static class ContentExtensions
+    {
+        public static string DescriptionSubstring(string description, int number = 150)
+        {
+            description = (description.Length < number
+                ? description
+                : description.Substring(0, description.Substring(0, number).LastIndexOf(" ")));
+            return description;
+        }
+    }
+}
