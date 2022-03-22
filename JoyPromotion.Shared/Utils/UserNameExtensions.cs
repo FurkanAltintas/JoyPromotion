902dd528677ac16dfdd8bdd34241f95827d52739
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-23 02:01:00 +0300

    UserNameExtensions Added

---
+++ JoyPromotion.Shared/Utils/UserNameExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace JoyPromotion.Shared.Utils
+{
+    public static class UserNameExtensions
+    {
+        public static string UserName(string userName)
+        {
+            if (string.IsNullOrEmpty(userName)) return "";
+            userName = userName.ToLower();
+            userName = userName.Trim();
+            userName = userName.Replace("İ", "I");
+            userName = userName.Replace("ı", "i");
+            userName = userName.Replace("ğ", "g");
+            userName = userName.Replace("Ğ", "G");
+            userName = userName.Replace("ç", "c");
+            userName = userName.Replace("Ç", "C");
+            userName = userName.Replace("ö", "o");
+            userName = userName.Replace("Ö", "O");
+            userName = userName.Replace("ş", "s");
+            userName = userName.Replace("Ş", "S");
+            userName = userName.Replace("ü", "u");
+            userName = userName.Replace("Ü", "U");
+            return userName;
+        }
+    }
+}
