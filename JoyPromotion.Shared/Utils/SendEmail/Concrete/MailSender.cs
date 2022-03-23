d8bf1db5ed61d2a723b332d0a470986d0c7bc574
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-23 23:36:23 +0300

    SendEmail Added

---
+++ JoyPromotion.Shared/Utils/SendEmail/Concrete/MailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace JoyPromotion.Shared.Utils.SendEmail.Concrete
+{
+    public class MailSender
+    {
+        const string SecurityKey = "0123456789ABCDEFGHJKLMNOPRSTUVWXYZ";
+
+        public string Key()
+        {
+            Random random = new Random();
+            int maxRand = SecurityKey.Length - 1;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int index = random.Next(maxRand);
+                stringBuilder.Append(SecurityKey[index]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public void Email(string email)
+        {
+            MailMessage mailMessage = new MailMessage();
+            string senderMail = "Email bilgimiz";
+            string senderPassword = "Password bilgimiz";
+            mailMessage.To.Add(email);
+            mailMessage.From = new MailAddress(senderMail);
+            mailMessage.Body = $"Aktivasyon Kodu {Key()}";
+            mailMessage.Subject = "Hesap Aktivasyon İşlemi";
+            SmtpClient smtp = new SmtpClient(Server.Gmail);
+            smtp.EnableSsl = true;
+            smtp.Port = (int)Server.Port.Gmail;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(senderMail, senderPassword);
+            smtp.Send(mailMessage);
+        }
+    }
+}
--- JoyPromotion.Shared/Utils/SendEmail/Port.cs
+++ JoyPromotion.Shared/Utils/SendEmail/Port.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JoyPromotion.Shared.Utils.SendEmail
+﻿namespace JoyPromotion.Shared.Utils.SendEmail
 {
     public class Server
     {
