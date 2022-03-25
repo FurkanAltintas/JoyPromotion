86831cb89512b47ad6a0d3ff94c791a59a26bac3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-25 20:55:17 +0300

    Contact DataAccess Added

---
+++ JoyPromotion.DataAccess/Concrete/Dapper/DpContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using JoyPromotion.DataAccess.Abstract;
+using JoyPromotion.Entities.Concrete;
+using JoyPromotion.Shared.DataAccess.Dapper;
+using System.Data;
+
+namespace JoyPromotion.DataAccess.Concrete.Dapper
+{
+    public class DpContactRepository : DpGenericRepository<Contact>, IContactRepository
+    {
+        public DpContactRepository(IDbConnection dbConnection) : base(dbConnection)
+        {
+        }
+    }
+}
