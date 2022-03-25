2ce6472b7b87c275f4216a8d99c59f8d4bc9b1c3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-25 19:55:22 +0300

    Contact Entity Added

---
+++ JoyPromotion.Entities/Concrete/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JoyPromotion.Entities.Concrete
+{
+    [Dapper.Contrib.Extensions.Table("Contacts")]
+    public class Contact
+    {
+        [Dapper.Contrib.Extensions.Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string HostName { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
