be8dfea9429b6a8b41e37079e14c865d151e3b4e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-03-25 19:55:37 +0300

    Captcha Added

---
+++ JoyPromotion.Shared/Utils/Security/Captcha/CaptchaModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoyPromotion.Shared.Utils.Security.Captcha
+{
+    public class CaptchaModel
+    {
+        [Required]
+        [StringLength(4)]
+        public string CaptchaCode { get; set; }
+    }
+}
