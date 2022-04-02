60e5dc9c3bffe45b57e863ad6b4e6556ab8f8e92
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 02:09:24 +0300

    UnitOfWork Dapper Mode Added

---
+++ JoyPromotion.DataAccess/Abstract/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JoyPromotion.DataAccess.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Categories { get; }
+        IContactRepository Contacts { get; }
+        IContentRepository Contents { get; }
+        IContentTagRepository ContentTags { get; }
+        IRoleRepository Roles { get; }
+        ISocialMediaRepository SocialMedias { get; }
+        ITagRepository Tags { get; }
+        IUserRepository Users { get; }
+        IUserSocialMediaRepository UserSocialMedias { get; }
+    }
+}
---
+++ JoyPromotion.DataAccess/Concrete/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using JoyPromotion.DataAccess.Abstract;
+using JoyPromotion.DataAccess.Concrete.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyPromotion.DataAccess.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbConnection _dbConnection;
+        private DpCategoryRepository _categoryRepository;
+
+
+        public UnitOfWork(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public ICategoryRepository Categories => _categoryRepository ?? new DpCategoryRepository(_dbConnection);
+
+        public IContactRepository Contacts => throw new NotImplementedException();
+
+        public IContentRepository Contents => throw new NotImplementedException();
+
+        public IContentTagRepository ContentTags => throw new NotImplementedException();
+
+        public IRoleRepository Roles => throw new NotImplementedException();
+
+        public ISocialMediaRepository SocialMedias => throw new NotImplementedException();
+
+        public ITagRepository Tags => throw new NotImplementedException();
+
+        public IUserRepository Users => throw new NotImplementedException();
+
+        public IUserSocialMediaRepository UserSocialMedias => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            _dbConnection.Dispose();
+        }
+    }
+}
