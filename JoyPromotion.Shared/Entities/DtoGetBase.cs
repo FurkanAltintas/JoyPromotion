312c55beb936284a57f14408c9a988c60cc5a267
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 02:10:18 +0300

    DtoGetBase Result Added

---
+++ JoyPromotion.Shared/Entities/DtoGetBase.cs
@@ -0,0 +1,10 @@
+﻿using JoyPromotion.Shared.Utils.Results.ComplexTypes;
+
+namespace JoyPromotion.Shared.Entities
+{
+    public abstract class DtoGetBase
+    {
+        public virtual ResultStatus ResultStatus { get; set; }
+        public virtual string Message { get; set; }
+    }
+}
