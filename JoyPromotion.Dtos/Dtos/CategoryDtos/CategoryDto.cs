005ddedf16d8c99312264b8d079b8feaf26a81f5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 02:09:48 +0300

    Dto ResultStatus Modified

--- JoyPromotion.Dtos/Dtos/CategoryDtos/CategoryDto.cs
+++ JoyPromotion.Dtos/Dtos/CategoryDtos/CategoryDto.cs
@@ -1,10 +1,13 @@
-﻿using JoyPromotion.Shared.Entities;
+﻿using JoyPromotion.Entities.Concrete;
+using JoyPromotion.Shared.Entities;
 
 namespace JoyPromotion.Dtos.Dtos
 {
-    public class CategoryDto : IDto
+    public class CategoryDto : DtoGetBase, IDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+
+        public Category Category { get; set; }
     }
 }
--- JoyPromotion.Dtos/Dtos/CategoryDtos/CategoryListDto.cs
+++ JoyPromotion.Dtos/Dtos/CategoryDtos/CategoryListDto.cs
@@ -1,10 +1,14 @@
-﻿using JoyPromotion.Shared.Entities;
+﻿using JoyPromotion.Entities.Concrete;
+using JoyPromotion.Shared.Entities;
+using System.Collections.Generic;
 
 namespace JoyPromotion.Dtos.Dtos
 {
-    public class CategoryListDto : IDto
+    public class CategoryListDto : DtoGetBase, IDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+
+        public IList<Category> Categories { get; set; }
     }
 }
