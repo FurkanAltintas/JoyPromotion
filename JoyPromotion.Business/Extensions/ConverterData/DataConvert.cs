698cb7deb462e87482d43698be2ec9a1df2527b4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-03 02:09:35 +0300

    Converter Mapper Added

---
+++ JoyPromotion.Business/Extensions/ConverterData/DataConvert.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using JoyPromotion.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyPromotion.Business.Extensions.ConverterData
+{
+    public class DataConvert<T> : IDataConvert<T> where T : class, IDto, new()
+    {
+        private readonly IMapper _mapper;
+
+        public DataConvert(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public object Convert(T data)
+        {
+            return _mapper.Map<object>(data);
+        }
+    }
+}
---
+++ JoyPromotion.Business/Extensions/ConverterData/IDataConvert.cs
@@ -0,0 +1,14 @@
+﻿using JoyPromotion.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyPromotion.Business.Extensions.ConverterData
+{
+    public interface IDataConvert<T> where T : class, IDto, new()
+    {
+        object Convert(T data);
+    }
+}
